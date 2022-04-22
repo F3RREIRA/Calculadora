6191e3acd61d99d56d1440c201bbab85848d98bc
RIVAILDO <80759812+F3RREIRA@users.noreply.github.com>
2022-04-21 21:50:09 -0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,15 +2,15 @@ var valor;
 
 var resultado;
 
-function botao(num){ 
-valor = document.calc.visor.value += num;
+function botao(num) {
+  valor = document.calc.visor.value += num;
 }
 
-function reseta(){ 
-document.calc.visor.value = '';
+function reseta() {
+  document.calc.visor.value = '';
 }
 
-function calcula(){ 
-resultado = eval(valor); 
-document.calc.visor.value = resultado.toLocaleString('pt-BR');
+function calcula() {
+  resultado = eval(valor);
+  document.calc.visor.value = resultado.toLocaleString('pt-BR');
 }
